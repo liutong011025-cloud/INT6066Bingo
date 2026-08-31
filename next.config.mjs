423a47e977fa8524ce71e24d6bedb4f61721a994
liutong011025-cloud <liutong011025@163.com>
2026-08-31 17:50:52 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  eslint: { ignoreDuringBuilds: true }
+  eslint: { ignoreDuringBuilds: true },
+  output: "export",
+  images: { unoptimized: true }
 };
 
 export default nextConfig;
